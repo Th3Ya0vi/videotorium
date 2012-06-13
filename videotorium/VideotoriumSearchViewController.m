e01894ecf3be72a7599476e860336eec6d7f8ba7
Zsombor Nagy <zsombor@nagy.com>
2012-06-13 10:21:07 +0200

    Scroll to the top of the search table when there are new results.

--- videotorium/VideotoriumSearchViewController.m
+++ videotorium/VideotoriumSearchViewController.m
@@ -85,6 +85,9 @@
     if (![_recordings isEqualToArray:recordings]) {
         _recordings = recordings;
         [self.tableView reloadData];
+        [self.tableView scrollToRowAtIndexPath:[NSIndexPath indexPathForRow:0 inSection:0]
+                              atScrollPosition:UITableViewScrollPositionTop
+                                      animated:NO];
     }
 }
 
